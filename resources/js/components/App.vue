8f097445ca8cbd9b61b971dcf5e254ed74a6f907
floravel <flo-hess1@web.de>
2021-04-01 12:38:08 +0200

    clean unused html, add Basic Vue Component and integrate it in app

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        Startpunkt
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav></Nav>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Nav from './Nav'
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        Nav
+    }
 }
 </script>
 
---
+++ resources/js/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>    ultrasack22</div>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+}
+</script>
+
+<style scoped>
+
+</style>
